c4dbfe27065c5621a16ca2f3db2ab7f94950bd11
Hrishikesh Kokate <hrishi5200@gmail.com>
2025-06-10 16:54:10 +0530

    Update say-hello-async.ts

--- netlify/functions/say-hello-async.ts
+++ netlify/functions/say-hello-async.ts
@@ -1,9 +1,24 @@
 import { type AsyncWorkloadConfig, type AsyncWorkloadEvent, asyncWorkloadFn } from '@netlify/async-workloads'
 
-export default asyncWorkloadFn((_: AsyncWorkloadEvent) => {
-  console.log('Hello, Async Workloads!')
+export default asyncWorkloadFn((a: AsyncWorkloadEvent) => {
+  console.log(`attempt: ${a.attempt}`)
+  throw new Error('failed on purpose')
 })
 
 export const asyncWorkloadConfig: AsyncWorkloadConfig = {
+  backoffSchedule(attempt) {
+    switch (attempt) {
+      case 1:
+        return '1s'
+      case 2:
+        return '5s'
+      case 3:
+        return '10s'
+      case 4:
+        return '30s'
+      default:
+        return '0s'
+    }
+  },
   events: ['say-hello']
 }
